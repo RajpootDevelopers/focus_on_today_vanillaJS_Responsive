aa9c1640195210072523050fd09682d4d7a6a651
Afaq Ahmad Rajpoot <ranag55db@gmail.com>
2024-05-31 13:09:13 +0500

    localStorage is in progress to implimenting

--- src/utils/Functionality.js
+++ src/utils/Functionality.js
@@ -3,40 +3,58 @@ const inputFields = document.querySelectorAll("input");
 const errorLabel = document.querySelector(".error-label");
 const progressValue = document.querySelector(".progress-value");
 
-inputFields.forEach((input)=>{
-    input.addEventListener("focus", ()=>{
-        errorLabel.style.visibility = "hidden";
-    })
-                    
-})
-
 let progValue = 0;
 
+// const allGoals = {};
 
-allCheckBoxes.forEach((box)=>{
-    box.addEventListener("click", ()=>{
+const allGoals = JSON.parse(localStorage.getItem("allGoals")) || {};
+
+inputFields.forEach((input) => {
+    input.value = allGoals[input.id]?.task || "";
+    input.addEventListener("focus", () => {
+        errorLabel.style.visibility = "hidden";
+    })
+    input.addEventListener("input", (e)=>{
+        allGoals[input.id] = {
+            task : e.target.value,
+            completed : false
+        };
+        localStorage.setItem("allGoals", JSON.stringify(allGoals))
+        // console.log(allGoals)
+    })
+
+})
+
+
+
+allCheckBoxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
         const inputArray = [...inputFields]
-        const allGoalsAdded = inputArray.every((element)=>{
-                        return element.value !== "";
-                    })
+        const allGoalsAdded = inputArray.every((element) => {
+            return element.value !== "";
+        })
         // if( box.nextElementSibling.value !== ""){
-            if(allGoalsAdded){
-                if(box.parentNode.classList.contains("completed")){
-                    box.parentNode.classList.remove("completed");
-                }else{
-                    box.parentNode.classList.add("completed");
-                }
-                errorLabel.style.visibility = "hidden";
-                progValue += 33.33
-            }else{
-                errorLabel.style.visibility = "visible";
+        if (allGoalsAdded) {
+            if (box.parentNode.classList.contains("completed")) {
+                box.parentNode.classList.remove("completed");
+                inputFields[index].disabled = false;
+                progValue -= 33.33;
+            } else {
+                box.parentNode.classList.add("completed");
+                inputFields[index].disabled = true;
+                (inputFields[index].disabled) ? inputFields[index].classList.add("input:disabled") : "";
+                progValue += 33.33;
             }
+            errorLabel.style.visibility = "hidden";
+            // progressValue.style.width += "33.33%";
+        } else {
+            errorLabel.style.visibility = "visible";
+        }
         // }
+        progressValue.style.width = `${progValue}%`;
     })
 })
-progressValue.style.width = progValue + "%";
 
-// console.log(allCheckBoxes)
 
 
 
